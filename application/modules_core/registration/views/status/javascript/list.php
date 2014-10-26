7aeea2efb4f7e0d0d622eb8230e99592206b3c19
albertusatria <albert@icubeonline.com>
2014-10-26 13:56:51 +0700

    create template for student status on registration data section

---
+++ application/modules_core/registration/views/status/javascript/list.php
@@ -0,0 +1,66 @@
+<!-- <link href="http://cdn.datatables.net/1.10.3/css/jquery.dataTables.css" rel="stylesheet"> -->
+<link href="<?php echo base_url();?>bracket/css/jquery.dataTables.custom.css" rel="stylesheet">
+<script src="<?php echo base_url();?>bracket/js/jquery-1.10.2.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/modernizr.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/jquery.sparkline.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/toggles.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/jquery.cookies.js"></script>
+
+<script src="<?php echo base_url();?>bracket/js/jquery-ui-1.10.3.min.js"></script>
+<script src="<?php echo base_url();?>bracket/js/jquery.mask.min.js"></script>
+
+<script src="<?php echo base_url()?>bracket/js/jquery.dataTables1.10.3.min.js"></script>
+<script src="<?php echo base_url()?>bracket/js/chosen.jquery.min.js"></script>
+<script src="<?php echo base_url()?>bracket/js/bootstrap-wizard.min.js"></script>
+<script src="<?php echo base_url()?>bracket/js/jquery.validate.min.js"></script>
+<script src="<?php echo base_url()?>bracket/js/dropzone.min.js"></script>
+<script src="<?php echo base_url()?>bracket/js/autoNumeric.js"></script>
+<script src="<?php echo base_url();?>bracket/js/custom.js"></script>
+
+<script type="text/javascript">
+        CI_ROOT = "<?=base_url() ?>";
+</script>
+
+<script type="text/javascript">
+// General Setting
+jQuery(document).ready(function() {
+
+	// With Form Validation Wizard
+	var $validator = jQuery("#search").validate({
+		highlight: function(element) {
+		  jQuery(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+		},
+		success: function(element) {
+		  jQuery(element).closest('.form-group').removeClass('has-error').addClass('validation-pass').css('margin-bottom', '12px');
+		}
+	});
+	
+    jQuery('#all-students').DataTable( {
+        initComplete: function () {
+            var api = this.api();
+ 
+            api.columns().indexes().flatten().each( function ( i ) {
+                var column = api.column( i );
+                var select = $('<select><option value="">Select By Above Information</option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $(this).val();
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
+    } );
+    
+    jQuery('#all-students tfoot th:last').after('<th>&nbsp;</th>');
+	
+});
+</script>
