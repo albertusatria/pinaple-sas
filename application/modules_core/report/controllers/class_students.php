3359cb126f4c9a4d11f0d6b16c89f50d971e962a
unknown <simoendem2@gmail.com>
2014-12-30 12:35:50 +0700

    Class & Extra Students Report Completed

---
+++ application/modules_core/report/controllers/class_students.php
@@ -0,0 +1,120 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once( APPPATH . 'modules_core/base/controllers/operator_base.php' );
+
+class Class_students extends Operator_base {
+	public function __construct() {
+		// call the controller construct
+		parent::__construct();
+
+		// load all the related model here
+		// load model
+		$this->load->model('m_reports');
+		$this->load->model('initiation/m_school_year');
+		$this->load->model('initiation/m_class_open');
+		$this->load->model('master/m_units');
+		$this->load->model('master/m_employees');
+
+		// load portal
+		$this->load->helper('text');
+
+		// page title
+		$this->page_title();
+
+		// active page
+		$active['parent_active'] = "general_reports";
+		$active['child_active'] = "class_students";
+		$this->session->set_userdata($active);		
+	}
+
+	// page title
+	public function page_title() {
+		$data['page_title'] = 'Class Students Report';
+		$this->session->set_userdata($data);
+	}
+
+	public function index()
+	{
+		// don't forget to give user_auth to every function before
+		$this->check_auth('R');
+
+		// two of these is a must
+		// menu
+		$data['menu'] = $this->menu();
+		// user detail
+		$data['user'] = $this->user;
+
+		//data
+		$data['active_school_year'] = $this->m_school_year->get_active_year();
+		$data['units'] = $this->m_units->get_all_unit_academic();
+
+		// get message flashdata		
+		$data['message'] = $this->session->flashdata('message');
+		$data['eror'] = $this->session->flashdata('eror');
+
+		$data['layout'] = "report/class_students/list_unit";
+		$data['javascript'] = "report/class_students/javascript/list_unit";
+		$this->load->view('dashboard/admin/template', $data);
+	}
+
+	public function class_list($unit_id = "")
+	{
+		// user_auth
+		$this->check_auth('R');
+
+		if ($unit_id == "") {
+			redirect('report/class_students/');
+		}
+
+		// load error message if any
+		$data['message'] = $this->session->flashdata('message');
+		$data['error'] = $this->session->flashdata('error');
+		// menu
+		$data['menu'] = $this->menu();
+		// user detail
+		$data['user'] = $this->user;
+		// get active school year information
+		$data['active_school_year'] = $this->m_school_year->get_active_year();
+		// get school year active id
+		$schyear = $data['active_school_year']->id;
+		// get list of class
+		$data['classes'] = $this->m_class_open->get_classes_list($unit_id, $schyear);
+		// get unit list
+		$data['unit'] = $this->m_units->get_all_unit_academic($unit_id);
+
+		// load template
+		$data['title']	 = "Class Students of unit ".$data['unit']->name;
+		$data['layout']  = "report/class_students/list_class";
+		$data['javascript'] = "report/class_students/javascript/list_class";
+		$this->load->view('dashboard/admin/template', $data);
+	}
+
+	public function student_list($unit_id = "", $class_id = "") {
+		// user_auth
+		$this->check_auth('R');
+
+		if ($unit_id == "") {
+			redirect('report/class_students/');
+		}
+
+		// menu
+		$data['menu'] = $this->menu();
+		// user detail
+		$data['user'] = $this->user;
+		// get active school year information
+		$data['active_school_year'] = $this->m_school_year->get_active_year();
+		// get school year active id
+		$schyear = $data['active_school_year']->id;
+		// get unit list
+		$data['unit'] = $this->m_units->get_unit_by_id($unit_id);
+		// get unit list
+		$data['class'] = $this->m_class_open->get_class($class_id);
+		// get unit list
+		$data['student'] = $this->m_reports->get_student_by_class_id($class_id);
+		// load template
+		$data['title']	 = "Class Open Setup PinapleSAS";
+		$data['layout']  = "report/class_students/list_student";
+		$data['javascript'] = "report/class_students/javascript/list_student";
+		$this->load->view('dashboard/admin/template', $data);
+	}
+
+}
---
+++ application/modules_core/report/views/extra_students/list_extra.php
@@ -0,0 +1,73 @@
+<div class="pageheader">
+  <h2><i class="fa fa-group"></i> Extra Students </h2>
+  <div class="breadcrumb-wrapper">
+    <span class="label">You are here:</span>
+    <ol class="breadcrumb">
+      <li><a href="<?php echo base_url();?>dashboard">Pinaple SAS</a></li>
+      <li>General Reports</li>
+      <li><a href="<?php echo base_url();?>report/extra_students">Extra Student</a></li>
+      <li class="active">Unit <?php echo $unit->name ?></li>
+    </ol>
+  </div>
+</div>
+    
+<div class="contentpanel">
+  
+  <?php if ($message != null ) : ?>
+  <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <strong>Well done!</strong> <?php echo $message; ?>
+    </div>
+  <?php endif ; ?>
+
+  <?php if ($error != null ) : ?>
+  <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+            <strong>Warning!</strong> <?php echo $error; ?>
+    </div>
+  <?php endif ; ?>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Extras on Unit <?php echo $unit->name ?></h3>
+    </div>
+    <div class="panel-body">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <th style="width:5%;">No</th>
+            <th style="width:25%;">Extra Name</th>
+            <th style="width:20%;">Homeroom Teacher 1</th>
+            <th style="width:20%;">Homeroom Teacher 2</th>
+            <th style="width:15%;">Half Period</th>
+          </thead>
+          <tbody>
+            <?php if (count($extras) > 0) : ?>
+              <?php $no = 1; foreach ($extras as $extra): ?>
+                  <tr>
+                      <td><?php echo $no; ?></td>
+                      <td><?php echo $extra->name; ?></td>
+                      <td><?php echo $extra->homeroom_1_name; ?></td>
+                      <td><?php echo $extra->homeroom_2_name; ?></td>
+                      <td>
+                         <a href="<?php echo base_url(); ?>report/extra_students/student_list/<?php echo $unit->id; ?>/<?php echo $extra->id;?>/1">
+                          One</a>                                         
+                      &nbsp;&nbsp; | &nbsp;&nbsp;
+                         <a href="<?php echo base_url(); ?>report/extra_students/student_list/<?php echo $unit->id; ?>/<?php echo $extra->id;?>/2">
+                          Two</a>                                         
+                      </td>
+                  </tr>
+              <?php $no++; endforeach ; ?>
+            <?php else : ?>
+                  <tr>
+                    <td colspan='6' align='center'>No Extra yet!</td>
+                  </tr>
+            <?php endif; ?>
+          </tbody>
+       </table>
+      </div><!-- table-responsive -->
+      <div class="clearfix mb30"></div>
+    </div><!-- panel-body -->
+  </div><!-- panel -->
+    
+</div><!-- contentpanel -->
